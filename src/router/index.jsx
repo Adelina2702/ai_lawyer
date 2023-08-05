4be13a1ec1b5563ec585a455419278ab568b9988
БЕГИМ <zarlykovabegim02@gmail.com>
2023-08-05 08:50:33 +0600

    start works

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { LogIn } from '../ui/pages/Auth/LogIn';
+import Home from '../ui/pages/Home/Home';
 
 export const Router = () => {
     return (
         <Routes>
-            <Route path='/' element={<LogIn/>} />
+            {/* <Route path='/' element={<LogIn />} /> */}
+            <Route path='/' element={<Home />} />
         </Routes>
     );
 }
